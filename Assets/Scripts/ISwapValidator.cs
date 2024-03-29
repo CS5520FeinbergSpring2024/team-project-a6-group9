21bc5dc86698205df8550eb135693b3220c28065
BinZ622114 <crystal622114@gmail.com>
2024-03-29 10:57:20 -0700

    Add SelectionSort scene

--- Assets/Scripts/ISwapValidator.cs
+++ Assets/Scripts/ISwapValidator.cs
@@ -2,4 +2,6 @@ using UnityEngine;
 
 public interface ISwapValidator {
     bool IsValidSwap(GameObject[] nodes, int index1, int index2);
+
+    bool IsValidSwapInSelectionSort(GameObject[] nodes, int index1, int index2, int startIndex);
 }
